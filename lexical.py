da6aeea266f135f112a47b9b2cf75a2a2e5dd6a9
Victor Souza <nutaro@protonmail.com>
2022-01-10 19:35:03 -0300

    making code more readable

--- lexical.py
+++ lexical.py
@@ -7,18 +7,18 @@ class Lexical:
         self.close_buffer = []
 
     def _analyze(self):
-        for x in self.values:
-            if x in self.open_close.keys():
-                self.open_buffer.append(x)
+        for char in self.values:
+            if char in self.open_close.keys():
+                self.open_buffer.append(char)
                 continue
-            if x in self.open_close.values():
-                self.close_buffer.append(x)
+            if char in self.open_close.values():
+                self.close_buffer.append(char)
 
     def validate(self) -> bool:
         self._analyze()
         if len(self.open_buffer) != len(self.close_buffer):
             return False
-        for x in range(0, len(self.open_buffer)):
+        for i in range(0, len(self.open_buffer)):
             _open = self.open_buffer.pop()
             _close = self.close_buffer.pop(0)
             if _close == self.open_close[_open]:
