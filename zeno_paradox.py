a078fd70a17622b51a26f1468207bbec04c8a0f5
Victor Souza <nutaro@protonmail.com>
2022-01-10 23:23:01 -0300

    fix achilles mechanics

--- zeno_paradox.py
+++ zeno_paradox.py
@@ -1,14 +1,22 @@
 class Runner:
     def __init__(self, position: int) -> None:
-        self.position = position
+        self._position = position
+
+    @property
+    def position(self) -> float:
+        return self._position
+
+    @position.setter
+    def position(self, value: int) -> None:
+        self._position = 1 / value
 
 
 def run() -> None:
-    achilles = Runner(0)
+    achilles = Runner(1)
     tortoise = Runner(1)
-    while achilles.position <= tortoise.position:
-        achilles.position = tortoise.position
-        tortoise.position += 0.5
+    for x in range(1, 50):
+        achilles.position = x
+        tortoise.position = x
         print(f"Achilles {achilles.position}")
         print(f"Tortoise {tortoise.position}")
 
